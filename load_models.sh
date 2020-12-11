5a1b84fbd2fe35d2a8b7489aabfe623826a882bf
indy1501 <indy.isb@gmail.com>
2020-12-10 19:13:48 -0800

    fix dependencies

--- load_models.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "./all_models" ]
-then 
-    echo "Directory all models already exists"
-else
-    mkdir all_models
-    wget "https://cmpe-257-alternus-vera.s3.amazonaws.com/feature_finders_toxicity_model"
-    mv feature_finders_toxicity_model ./all_models/
-fi
